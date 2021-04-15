cf2a60fd1063fa692f3c15b785052c4f61086ec8
Agnieszka Witek <witek.aga@gmail.com>
2021-04-15 19:55:41 +0200

    Try to filter trips

--- src/components/features/TripListOptions/TripListOptions.js
+++ src/components/features/TripListOptions/TripListOptions.js
@@ -4,20 +4,25 @@ import styles from './TripListOptions.scss';
 
 import {Row, Col} from 'react-flexbox-grid';
 
+
+
 class TripListOptions extends React.Component {
   handleTags(tag, checked){
     if(checked) {
       console.log('Adding tag', tag);
       // TODO - use action dispatcher from props
+      this.props.addTag(tag);
     } else {
       console.log('Removing tag', tag);
       // TODO - use action dispatcher from props
+      this.props.removeTag(tag);
     }
   }
 
   handleDuration(type, value){
     console.log('Changing duration', type, value);
     // TODO - use action dispatcher from props
+    this.props.changeDuration(value);
   }
 
   handleSearch(phrase){
@@ -69,10 +74,14 @@ class TripListOptions extends React.Component {
   }
 }
 
+
 TripListOptions.propTypes = {
   tags: PropTypes.object,
   filters: PropTypes.object,
   changeSearchPhrase: PropTypes.func,
+  addTag: PropTypes.func,
+  removeTag: PropTypes.func,
+  changeDuration: PropTypes.func,
 };
 
 export default TripListOptions;
