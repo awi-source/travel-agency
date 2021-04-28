1bbe117b5722cd0a0848f0da5232258a1e36a469
Agnieszka Witek <witek.aga@gmail.com>
2021-04-28 22:49:22 +0200

    Start working on OrderOption

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,11 +2,22 @@ import React from 'react';
 import {Grid, Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import PropTypes from 'prop-types';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
+console.log(pricing);
 
 const OrderForm = ({options, tripCost}) => (
+  
   <Grid>
     <Row>
+      {pricing.map(option => 
+        
+        <Col key={option.id} md={4} >
+          <OrderOption>{option}</OrderOption>
+        </Col>
+    
+      )}
       <Col xs={12}>
         <OrderSummary tripCost={tripCost} options={options} />
       </Col>
---
+++ src/components/features/OrderOption/OrderOption.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './OrderOption.scss';
+// import {Grid, Row, Col} from 'react-flexbox-grid';
+// import OrderSummary from '../OrderSummary/OrderSummary';
+// import PropTypes from 'prop-types';
+
+
+const OrderOption = () => (
+  <div className={styles.component}>
+    <h3 className={styles.title}></h3>
+  </div>
+);
+
+// OrderForm.propTypes = {
+ 
+// };
+export default OrderOption;
